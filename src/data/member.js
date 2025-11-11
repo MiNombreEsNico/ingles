e93f864783f5d476d4b3846e99143950f6ec96ed
Miguel Nicolás Basualdo Martinez <niicomartiinez22@gmail.com>
2025-11-10 23:14:00 -0300

    terminado

---
+++ src/data/member.js
@@ -0,0 +1,20 @@
+export const member = [
+    {
+        name: "Nicolás",
+        image: "/img/profile/nico.avif",
+        description: "Full Stack Developer",
+        cv: "docs/cv/1.pdf"
+    },
+    {
+        name: "Gerardo",
+        image: "/img/profile/gera.avif",
+        description: "Full Stack Developer",
+        cv: "docs/cv/2.pdf"
+    },
+    {
+        name: "Santino",
+        image: "/img/profile/santino.avif",
+        description: "Full Stack Developer",
+        cv: "docs/cv/3.pdf"
+    },
+];
---
+++ src/data/products.js
@@ -0,0 +1,16 @@
+export const products = [
+    {
+        title: "Easily",
+        logo: "/img/logos/logo_easily.png",
+        image: "/img/product-easily.png",
+        description: "The best platform to take classes online.",
+        link: "/easily",
+    },
+    {
+        title: "Poepsa",
+        logo: "/img/logos/logo_poepsa.svg",
+        image: "/img/product-poepsa.png",
+        description: "The best platform to manage your business.",
+        link: "/poepsa",
+    },
+];
